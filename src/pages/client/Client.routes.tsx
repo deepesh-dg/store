abd39e8f782dd4975d70726521ca22f37bfd7653
Deepesh Gupta <34032722+deepesh-dg@users.noreply.github.com>
2023-01-02 13:41:12 +0530

    cart added to route

--- src/pages/client/Client.routes.tsx
+++ src/pages/client/Client.routes.tsx
@@ -4,10 +4,12 @@ import Client from './Client';
 
 const HomeComponent = lazy(() => import('./pages/home/Home'));
 const UsersComponent = lazy(() => import('./pages/users/Users'));
+const CartComponent = lazy(() => import('./pages/cart/Cart'));
 
 export const routes: JSX.Element = (
 	<Route path='' element={<Client />}>
 		<Route path='' element={<HomeComponent />} />
 		<Route path='users' element={<UsersComponent />} />
+		<Route path='cart' element={<CartComponent />} />
 	</Route>
 );
